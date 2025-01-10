fbebb6a19397490738beef2c34dc72f63b6e3e2c
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2025-01-10 22:57:18 +0530

    update rollTheDice app

--- rollTheDice05/src/App.tsx
+++ rollTheDice05/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState, useRef } from 'react';
+import type { PropsWithChildren } from 'react';
 import {
+  Animated,
   ImageSourcePropType,
   SafeAreaView,
   ScrollView,
@@ -26,10 +27,30 @@ type DiceProps = PropsWithChildren<{
 
 
 const Dice = ({imageUrl}: DiceProps): React.JSX.Element => {
+
+  const animation = useRef(new Animated.Value(1)).current;
+  
+  // Trigger scale animation when the dice rolls
+   const startAnimation = () => {
+    Animated.sequence([
+      Animated.timing(animation, {
+        toValue: 1.2,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animation, {
+        toValue: 1,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
+
+
   return (
-    <View style={styles.imageContainer}>
+    <Animated.View style={{ transform: [{ scale: animation }] }}>
       <Image source={imageUrl} style={styles.diceImage} />
-    </View>
+    </Animated.View>
   )
 }
 
@@ -67,13 +88,17 @@ function App(): React.JSX.Element {
     }
   }
   
+  
   return (
     <View style={styles.container}>
       <View style={styles.diceContainer}>
         <Dice imageUrl={diceImage} />
         <Pressable 
-        style={styles.button}
-        onPress={rolltheDiceOnTap}
+          style={({ pressed }) => [
+            styles.button,
+            pressed && { backgroundColor: '#7D91E0' },
+          ]}
+          onPress={rolltheDiceOnTap}
         >
           <Text style={styles.buttonText}>Roll The Dice</Text>
         </Pressable>
@@ -92,9 +117,6 @@ const styles = StyleSheet.create({
   diceContainer: {
     alignItems: 'center',
   },
-  imageContainer: {
-    marginVertical: 20,
-  },
   diceImage: {
     width: 200,
     height: 200,
@@ -105,6 +127,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 8,
     elevation: 4,
+    marginTop: 20,
   },
   buttonText: {
     fontSize: 18,
